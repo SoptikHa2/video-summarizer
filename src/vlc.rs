cfd6941fa1a22053e8117bbb6bcfc81364646eab
Petr Šťastný <petr.stastny01@gmail.com>
2020-01-01 22:47:21 +0100

    Cleaned vlc controller
    Removed unwraps, added error propagation instead

--- src/vlc.rs
+++ src/vlc.rs
@@ -1,9 +1,7 @@
 use crate::video_processing::{Video, VideoSource};
 
-use guid_create::GUID;
 use std::fmt::{self, Display};
-use std::fs::{File, OpenOptions};
-use std::io::{Read, Write};
+use std::io::{BufWriter, Write};
 use std::process::{Command, Stdio};
 use std::thread;
 use std::time::Duration;
@@ -28,7 +26,7 @@ use telnet::{Telnet, TelnetEvent};
 /// 
 /// We can easily access VLC via telnet.
 pub struct VlcController {
-    filename: String,
+    source: VideoSource,
     video_frames: Vec<bool>,
     video_seconds: f32,
     speedup_loud: f32,
@@ -41,36 +39,12 @@ impl VlcController {
     /// If the video was sent to stdin, save it to named pipe.
     /// This will fail if `mkfifo` doesn't exist.
     pub fn new(video_info: Video, speedup_loud: f32, speedup_silent: f32) -> Result<VlcController, VlcControllerError> {
-        let filename: String;
-        if let VideoSource::StdinStream(stream) = video_info.source {
-            // If user passed video as stream into stdin, save it to
-            // temporary fifo file
-            filename = GUID::rand().to_string();
-            Command::new("mkfifo").arg(&filename).output()?;
-            // Write to fifo in new thread
-            // TODO: Get rid of panic inside the thread?
-            let fifo_filename_cloned = filename.clone();
-            thread::spawn(move || {
-                let mut fifo = OpenOptions::new()
-                    .write(true)
-                    .open(fifo_filename_cloned)
-                    .unwrap();
-                fifo.write_all(&stream[..]).unwrap();
-            });
-        } else {
-            if let VideoSource::FilePath(file) = video_info.source {
-                filename = file;
-            } else {
-                panic!("Fatal error: missing video information for VLC process. Open bug report at >>https://github.com/soptikha2/video-summarizer<<");
-            }
-        }
-
         if video_info.silent_frames.is_none() || video_info.length_seconds.is_none() {
             return Err(VlcControllerError::NotEnoughInfo);
         }
 
         Ok(VlcController {
-            filename,
+            source: video_info.source,
             video_frames: video_info.silent_frames.unwrap(),
             video_seconds: video_info.length_seconds.unwrap(),
             speedup_loud,
@@ -85,89 +59,103 @@ impl VlcController {
     pub fn start(mut self) -> Result<(), VlcControllerError> {
         let telnet_password: String = self.generate_telnet_password(128);
 
+        let parameter_source = match &self.source {
+            VideoSource::FilePath(s) => s,
+            VideoSource::StdinStream(_) => "-",
+        };
+        let parameter_stdin = match &self.source {
+            VideoSource::FilePath(_) => Stdio::null(),
+            VideoSource::StdinStream(_) => Stdio::piped(),
+        };
         // Start VLC with parameter -Irc
         let vlc_command = Command::new("vlc")
             .arg("-I telnet")
             .arg("--telnet-password")
             .arg(&telnet_password)
-            .arg(&self.filename)
+            .arg(parameter_source)
+            .stdin(parameter_stdin)
             .stdout(Stdio::null())
-            .stdin(Stdio::null()) //todo pipe from stdin
             .spawn()?;
+        match &self.source {
+            VideoSource::StdinStream(stream) => {
+                // Send stdin contents to the process
+                let mut command_stdin = vlc_command.stdin.unwrap();
+                let mut writer = BufWriter::new(&mut command_stdin);
+                writer.write_all(stream)?;
+            }
+            _ => {}
+        }
 
         // Wait a bit
         // TODO: sane solution
         thread::sleep(Duration::from_millis(200));
         
             // Initialize telnet
-            self.initialize_vlc_telnet(telnet_password);
+            self.initialize_vlc_telnet(telnet_password)?;
             thread::sleep(Duration::from_millis(500));
-            // let mut vlc_stdin = vlc_command.stdin.unwrap();
-            // let mut vlc_stdout = vlc_command.stdout.unwrap();
             loop {
                // Repeat this every 200ms...
                 thread::sleep(Duration::from_millis(200));
                 // Ask vlc if the video is playing right now.
-                self.send_to_vlc_via_telnet("is_playing");
+                self.send_to_vlc_via_telnet("is_playing")?;
                 thread::sleep(Duration::from_millis(200));
-                let is_playing = VlcController::parse_vlc_response_to_usize(self.receive_response_from_telnet()).unwrap();
-                if is_playing == 0 {
+                let is_playing = VlcController::parse_vlc_response_to_usize(self.receive_response_from_telnet()?);
+                if is_playing.is_none() { break; }
+                if is_playing.unwrap() == 0 {
                     // Wait until user starts playing the video
                     continue;
                 }
                 // Ask vlc for time
-                let time_seconds = self.get_current_time().unwrap();
-                let speedup = self.get_speedup_for_current_second(time_seconds);
+                let time_seconds = self.get_current_time();
+                if time_seconds.is_none() { break; }
+                let speedup = self.get_speedup_for_current_second(time_seconds.unwrap());
                 // Set speedup rate
-                self.send_to_vlc_via_telnet(&format!("rate {}", speedup));
-                eprintln!("Set rate to {}", speedup);
+                self.send_to_vlc_via_telnet(&format!("rate {}", speedup))?;
             }
 
         // Wait for it to finish.
         // The thread will end when vlc process exits.
-        //vlc_thread.join();
-
-        // TODO: unwrap -> result, when reading fails, keep reading; there are more rows.
 
         Ok(())
     }
 
-    fn initialize_vlc_telnet(&mut self, password: String) {
-        self.telnet_connection = Some(Telnet::connect(("localhost", 4212), 256).unwrap());
+    fn initialize_vlc_telnet(&mut self, password: String) -> Result<(), VlcControllerError> {
+        self.telnet_connection = Some(Telnet::connect(("localhost", 4212), 256)?);
         // Send password
-        self.telnet_connection.as_mut().unwrap().write(format!("{}\n", password).as_bytes()).unwrap();
+        self.telnet_connection.as_mut().unwrap().write(format!("{}\n", password).as_bytes())?;
         // Ignore welcoming messages
         for _ in 0..2 {
-            eprintln!("Ignoring: {}", self.receive_response_from_telnet());
+            self.receive_response_from_telnet()?;
         }
+
+        Ok(())
     }
 
-    fn send_to_vlc_via_telnet(&mut self, command: &str) {
-        self.telnet_connection.as_mut().unwrap().write(format!("{}\n", command).as_bytes()).unwrap();
+    fn send_to_vlc_via_telnet(&mut self, command: &str) -> Result<(), VlcControllerError> {
+        self.telnet_connection.as_mut().unwrap().write(format!("{}\n", command).as_bytes())?;
+        Ok(())
     }
 
     /// Blocking read from telnet, unspecified amount of bytes.
     /// This might not read to end, see `receive_response_from_telnet_until_found`
-    fn receive_response_from_telnet(&mut self) -> String {
-        let event = self.telnet_connection.as_mut().unwrap().read().unwrap();
+    fn receive_response_from_telnet(&mut self) -> Result<String, VlcControllerError> {
+        let event = self.telnet_connection.as_mut().unwrap().read()?;
 
         match event {
             TelnetEvent::Data(buffer) => {
-                return String::from_utf8((*buffer).to_vec()).unwrap();
+                return Ok(String::from_utf8((*buffer).to_vec())?);
             }
             _ => { return self.receive_response_from_telnet(); }
         }
     }
 
     /// Keep blocking-reading from telnet until everything specified in required_contents was read.
-    fn receive_response_from_telnet_until_found(&mut self, required_contents: Vec<&str>) -> String {
+    fn receive_response_from_telnet_until_found(&mut self, required_contents: Vec<&str>) -> Result<String, VlcControllerError> {
         let mut seen: Vec<bool> = required_contents.iter().map(|_| false).collect();
 
         let mut response = String::new();
         while seen.iter().any(|s| *s == false) {
-            eprintln!("Still waiting for {:?}", required_contents.iter().zip(&seen).filter(|(_, found)| !*found).map(|(contents, _)| contents));
-
-            let newest_response = self.receive_response_from_telnet();
+            let newest_response = self.receive_response_from_telnet()?;
             for i in 0..required_contents.len() {
                 if seen[i] {
                     continue;
@@ -178,7 +166,7 @@ impl VlcController {
             }
             response.push_str(&newest_response);
         }
-        response
+        Ok(response)
     }
 
     /// Take vlc response line which looks like this:
@@ -206,10 +194,9 @@ impl VlcController {
     /// If any of this is missing, return none.
     fn get_current_time(&mut self) -> Option<f32> {
         // Send stats and info commands
-        self.send_to_vlc_via_telnet("stats");
-        self.send_to_vlc_via_telnet("info");
-        let mut response: String = self.receive_response_from_telnet_until_found(vec!["frames displayed", "frames lost", "Frame rate"]);
-        eprintln!("Time output:\n{}\n--END_TIME_OUTPUT--", response);
+        self.send_to_vlc_via_telnet("stats").ok()?;
+        self.send_to_vlc_via_telnet("info").ok()?;
+        let response: String = self.receive_response_from_telnet_until_found(vec!["frames displayed", "frames lost", "Frame rate"]).ok()?;
         let frames_displayed_line: &str = response.lines().filter(|line| line.contains("frames displayed")).next()?;
         let frames_displayed = frames_displayed_line.replace(" ", "").split(":").last()?.parse::<usize>().ok()?;
         let frames_lost_line: &str = response.lines().filter(|line| line.contains("frames lost")).next()?;
@@ -249,7 +236,7 @@ impl VlcController {
         let allowed_characters: Vec<char> = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM;',./?><|\":}{P})(*&^%$#@!)".chars().collect();
 
         let mut password = String::with_capacity(length);
-        for i in 0..length {
+        for _ in 0..length {
             // We don't need cryptographical security
             // If anyone wants to have it, please submit a pull request
             let rnd = rand::random::<usize>() % allowed_characters.len();
@@ -263,10 +250,16 @@ impl VlcController {
 pub enum VlcControllerError {
     ExternalCommandError(std::io::Error),
     NotEnoughInfo,
+    CommunicationErrorWithVlc(std::string::FromUtf8Error),
 }
 impl From<std::io::Error> for VlcControllerError {
     fn from(e: std::io::Error) -> Self {
         VlcControllerError::ExternalCommandError(e)
+    }
+}
+impl From<std::string::FromUtf8Error> for VlcControllerError {
+    fn from(err: std::string::FromUtf8Error) -> Self {
+        VlcControllerError::CommunicationErrorWithVlc(err)
     }
 }
 impl Display for VlcControllerError {
@@ -278,6 +271,9 @@ impl Display for VlcControllerError {
                 VlcControllerError::NotEnoughInfo => format!(
                     "We don't have enough info about video to start playing it. Use `video.analyze_sound().`"
                 ),
+                VlcControllerError::CommunicationErrorWithVlc(e) => format!(
+                    "Something went wrong while communicating with VLC: {}", e
+                )
             };
         write!(f, "An error occured while processing video: {}", reason)
     }
